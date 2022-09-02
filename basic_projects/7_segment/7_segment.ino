944495b4b9b8fba4e292013234b862a78df1e837
Neil Arora <neilarora2008@gmail.com>
2022-09-02 11:10:01 +0530

    Create 7_segment.ino

---
+++ basic_projects/7_segment/7_segment.ino
@@ -0,0 +1,141 @@
+int a = 6;
+int b = 5;
+int c = 2;
+int d = 3;
+int e = 4;
+int f = 7;
+int g = 8;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(a,OUTPUT);
+pinMode(b,OUTPUT);
+pinMode(c,OUTPUT);
+pinMode(d,OUTPUT);
+pinMode(e,OUTPUT);
+pinMode(f,OUTPUT);
+pinMode(g,OUTPUT);
+}
+
+void ab(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);  
+}
+
+void bc(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);  
+}
+
+void cd(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);  
+}
+
+void de(){
+  digitalWrite(a,HIGH);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);  
+}
+
+void ef(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);  
+}
+
+void fg(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,HIGH);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);  
+}
+
+void gh(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);  
+}
+
+void hi(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);  
+}
+
+void ij(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);  
+}
+
+void jk(){
+  digitalWrite(a,LOW);
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,HIGH);  
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  jk();
+  delay(1000);
+  ab();
+  delay(1000);
+  bc();
+  delay(1000);
+  cd();
+  delay(1000);
+  de();
+  delay(1000);
+  ef();
+  delay(1000);
+  fg();
+  delay(1000);
+  gh();
+  delay(1000);
+  hi();
+  delay(1000);
+  ij();
+  delay(1000); 
+}
